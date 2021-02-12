ca7e7942e83b95e94dc6b03b22b313351db3b26c
albparmor2 <albparmor2@alum.us.es>
2021-02-12 10:11:44 +0000

    Añadiendo los ficheros para heroku
    Añadiendo los ficheros necesarios para heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -168,6 +168,10 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL = 'https://egc-app-albparmor2-1230.herokuapp.com'
+
+APIS = {}
+
 try:
     from local_settings import *
 except ImportError:
@@ -183,3 +187,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
